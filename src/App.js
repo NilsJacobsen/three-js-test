03d42cc9b0e1bab1d4fec1025b83a6ce66b3f484
Nils Jacobsen <nils.jacobsen98@gmail.com>
2022-03-29 10:34:48 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import styles from './App.module.css';
+import TrainStation from "./Trainstation";
+import { Physics } from '@react-three/cannon'
+import { Cube, Plane, Plattform } from "./User";
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <Canvas 
+        shadowMap 
+        orthographic 
+        camera={{ zoom: 13, position: [200, 200, 200] }}
+      >
+        <Physics >
+          <TrainStation/>
+          {/* <Plane /> */}
+          <Plattform args={[20, 1, 60]}/>
+          <Cube receiveShadow castShaddow />
+        </Physics>
+        <directionalLight
+          intensity={0.8}
+          castShadow
+          shadow-mapSize-height={512}
+          shadow-mapSize-width={512}
+          position={[-200, 200, 200]}
+        />
+        <ambientLight intensity={0.1} />
+      </Canvas>
+    </div>
+  )
+};
+
+export default App;
---
+++ src/User.js
@@ -0,0 +1,71 @@
+import { usePlane, useBox } from '@react-three/cannon'
+import { useFrame } from '@react-three/fiber'
+import { useEffect, useRef } from 'react'
+
+export function Cube(props) {
+  const [ref, api] = useBox(() => ({ mass: 5, type: "Dynamic", position: [0, 20, 0], ...props }))
+  const code = useCodes();
+  const moveForward = (distance) => {
+    const speed = distance*100 / (-1);
+    api.velocity.set(speed,0,speed);
+  }
+  const moveRight = (distance) => {
+    const speed = distance*100 / (-1);
+    api.velocity.set(-speed,0,speed);
+  }
+  useFrame((_, delta) => {
+    const speed = code.current.has('ShiftLeft') ? 5 : 2
+    if (code.current.has('KeyW')) moveForward(delta * speed)
+    if (code.current.has('KeyA')) moveRight(-delta * speed)
+    if (code.current.has('KeyS')) moveForward(-delta * speed)
+    if (code.current.has('KeyD')) moveRight(delta * speed)
+  })
+  return (
+    <mesh ref={ref}>
+      <boxGeometry />
+      <meshLambertMaterial attach='material' color='white' />
+    </mesh>
+  )
+}
+
+export function Plane(props) {
+  const [ref] = usePlane(() => ({ transparent: true, rotation: [-Math.PI / 2, 0, 0], position: [0,1,0], ...props }))
+  return (
+    <mesh ref={ref}>
+      <planeGeometry args={[20, 60]} />
+      <meshPhongMaterial color="#ffffff"  transparent />
+    </mesh>
+  )
+}
+
+const useCodes = () => {
+  const codes = useRef(new Set())
+  useEffect(() => {
+    const onKeyDown = (e) => codes.current.add(e.code)
+    const onKeyUp = (e) => codes.current.delete(e.code)
+    window.addEventListener('keydown', onKeyDown)
+    window.addEventListener('keyup', onKeyUp)
+    return () => {
+      window.removeEventListener('keydown', onKeyDown)
+      window.removeEventListener('keyup', onKeyUp)
+    }
+  }, [])
+  return codes
+}
+  
+
+export function Plattform(props) {
+  const [ref, api] = useBox(() => ({ 
+    mass: 5, 
+    type: "Static", 
+    position: [0, 1, 0], 
+    ...props }
+  ))
+
+  return (
+    <mesh ref={ref}>
+      <boxGeometry args={[20, 1, 60]}/>
+      <meshLambertMaterial color="#ffffff" opacity={0} transparent />
+    </mesh>
+  )
+}
